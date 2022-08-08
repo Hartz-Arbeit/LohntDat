b37fb760b5e234f993a7fe5575a636e6e74f7fbf
jpv-os <63935349+jpv-os@users.noreply.github.com>
2022-08-08 20:21:45 +0200

    Datenmodell ALG2-Formular
    Typescript Modell für die Eingabetabelle aus dem ALG2-Rechner von https://tacheles-sozialhilfe.de/files/Aktuelles/2022/ALG2-Berechnung-ver-5-0.xlsx
    
    Unabhängig vom gewählten Framework sollte ein klar verständliches und stark typisiertes Datenmodell etabliert werden, welches von der App verwendet wird, um zum Beispiel ein Eingabeformular für den User zu validieren. 
    
    Die Variablen wurden hier zwar wohlüberlegt, aber dennoch zähneknirschend, in deutsch benannt, um Unklarheiten durch Übersetzung bei dem komplexen Formular zu vermeiden. Sobald die App umgesetzt wird, sollte das möglicherweise geändert werden. Für Klarheit und Diskussionsgrundlage zu Beginn des Projektes hilft es zunächst.

---
+++ alg2-formular.ts
@@ -0,0 +1,116 @@
+/*
+ * Datenmodell für die Eingabetabelle aus dem ALG2-Rechner von https://tacheles-sozialhilfe.de/files/Aktuelles/2022/ALG2-Berechnung-ver-5-0.xlsx
+ */
+
+export interface ALG2Formular {
+  beginnBewilligungszeitraum: Date;
+  antragsteller: LeistungsberechtigtePerson;
+  partnerInBG?: LeistungsberechtigtePerson;
+  kinder?: LeistungsberechtigtePerson[];
+  unterkunft: Unterkunft;
+  personenImHaushaltAusserhalbBG: number;
+}
+
+export interface LeistungsberechtigtePerson {
+  geburtsdatum: Date;
+  kinderAusserhalbBG: boolean;
+  alleinerziehend: boolean;
+  erwerbsunfaehig: boolean;
+  mehrbedarf: Mehrbedarf;
+  einkommen: Einkommen;
+  absetzung: Absetzung;
+}
+
+export interface Mehrbedarf {
+  ernaehrung?: MehrbedarfErnaehrung;
+  schwangerschaft?: MehrbedarfSchwangerschaft;
+  teilhabeArbeitsleben: boolean;
+  besondererBedarf?: number;
+}
+
+export enum MehrbedarfErnaehrung {
+  STUFE_EINS = 1,
+  STUFE_ZWEI = 2,
+}
+
+export interface MehrbedarfSchwangerschaft {
+  entbindungstermin: Date;
+  betrag: number;
+}
+
+export interface Einkommen {
+  erwerbBrutto: number;
+  erwerbNetto: number;
+  selbststaendigkeit?: number;
+}
+
+export interface Absetzung {
+  gezahlteEinkommenssteuer?: number;
+  versicherungenAusserhalbErwerb: {
+    krankenversicherung?: number;
+    pflegeversicherung?: number;
+    rentenversicherung?: number;
+    privateAltersvorsorge?: number;
+    zvkBeitrag?: number;
+  }
+  arbeitstageImMonat?: number;
+  arbeitstageImMonatMit12StundenAbwesenheit?: number;
+  entfernungWohnungArbeitsplatz?: number;
+  privatePflichtversicherungenJahresbeitraege?: number;
+  sonstigeVersicherungenMonatsbeitraege?: number;
+  gewerkschaftsbeitraege?: number;
+  unterhaltsverpflichtungen?: number;
+  arbeitsmittel?: number;
+  werbungskosten?: number;
+  riestervertrag: boolean;
+  zusatzbeitragKrankenversicherung?: number;
+  priviligiertesEinkommen?: number;
+  taschengeldAusFreiwilligenDiensten?: number;
+  arbeitslosengeldEins?: number;
+  mittelwertKindergeldFuerAlleKinderInBG?: number;
+  unterhalt?: number;
+  unterhaltsvorschuss?: number;
+  wohngeldOderMischhaushalt?: number;
+  ausbildungsfoerderung?: number;
+  altersrente?: number;
+  leistungenRentenUndKrankversicherungen?: number;
+  sonstigesEinkommen?: number;
+  mieteinnahmen?: number;
+  sonstigesEinkommenKinderzuschlagBetreuungsgeldEtc?: number;
+  elterngeld?: {
+    betrag: number;
+    durchschnittlichesMonatlichesEinkommen: number;
+    verlaengerungsoptionGenutzt: boolean;
+    freibetrag?: number;
+  };
+  ueberhangKindergeld?: number;
+}
+
+export interface Unterkunft {
+  mietstufe: Mietstufe | MietstufenBerechnung;
+  kaltmiete: number;
+  nebenkosten: number;
+  heizkosten: number;
+  zentraleWasserversorgung?: number;
+}
+
+export enum Mietstufe {
+  STUFE_EINS = 1,
+  STUFE_ZWEI = 2,
+  STUFE_DREI = 3,
+  STUFE_VIER = 4,
+  STUFE_FUENF = 5,
+  STUFE_SECHS = 6,
+  STUFE_SIEBEN = 7
+}
+
+export interface MietstufenBerechnung {
+  bundesland: string;
+  wohnort: string;
+  wohnverhaeltnis: Wohnverhaeltnis;
+}
+
+export enum Wohnverhaeltnis {
+  MIETE = "MIETE",
+  EIGENTUM = "EIGENTUM"
+}
